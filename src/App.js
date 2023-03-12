3289d5e7d4763191e5bbbbb1cd73b1d66706b6f6
Mthokozisi Martin <mbeemtk@gmail.com>
2023-03-12 04:26:21 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ModalFooter, Modal, ModalBody, ModalHeader } from 'reactstrap';
+
+//access images using   <img src={process.env.PUBLIC_URL + '/assets/image.jpg'} /> 
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Button
+    color="danger"
+    onClick={function noRefCheck(){}}
+  >
+    Click Me
+  </Button>
+  <Modal toggle={function noRefCheck(){}}>
+    <ModalHeader toggle={function noRefCheck(){}}>
+      Modal title
+    </ModalHeader>
+    <ModalBody>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <br />
+      <Button
+        color="success"
+        onClick={function noRefCheck(){}}
+      >
+        Show Nested Modal
+      </Button>
+      <Modal toggle={function noRefCheck(){}}>
+        <ModalHeader>
+          Nested Modal title
+        </ModalHeader>
+        <ModalBody>
+          Stuff and things
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={function noRefCheck(){}}
+          >
+            Done
+          </Button>
+          {' '}
+          <Button onClick={function noRefCheck(){}}>
+            All Done
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </ModalBody>
+    <ModalFooter>
+      <Button
+        color="primary"
+        onClick={function noRefCheck(){}}
+      >
+        Do Something
+      </Button>
+      {' '}
+      <Button onClick={function noRefCheck(){}}>
+        Cancel
+      </Button>
+    </ModalFooter>
+  </Modal>
+  </div>
   );
 }
 
